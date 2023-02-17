41357696199c1b825e2736f72053d9b2888288f7
Joe <joe830507@gmail.com>
2023-02-17 15:46:01 +0800

    Update OrderDisplayDto.java
    加入日期的格式

--- src/main/java/com/order/OrderDisplayDto.java
+++ src/main/java/com/order/OrderDisplayDto.java
@@ -2,6 +2,8 @@ package com.order;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,5 +16,6 @@ public class OrderDisplayDto {
 	private Long productId;
 	private Long accountId;
 	private Integer purchasedCount;
+	@JsonFormat(pattern = "yyyy-MM-dd")
 	private Date purchasedDate;
 }
